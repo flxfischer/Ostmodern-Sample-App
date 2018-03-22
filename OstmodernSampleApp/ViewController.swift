3565fdaca5fe364d14d4ff62811f1d6e8c75f15f
Felix Fischer <felix@urbandisruptive.de>
2018-03-22 18:37:28 +0100

    Initial Commit

---
+++ OstmodernSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OstmodernSampleApp
+//
+//  Created by Felix Fischer on 22.03.18.
+//  Copyright © 2018 Felix Fischer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
